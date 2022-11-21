0f327cc6ff62f2ff1fe493ccab0c39cede76e864
DagonChernovski <95167373+DagonChernovski@users.noreply.github.com>
2022-11-22 03:13:47 +0600

    new stage

--- CompareThemAll.java
+++ CompareThemAll.java
@@ -28,16 +28,27 @@ public class CompareThemAll {
             for (j=i+1; j<n; j++) {
                 if (a[j].key<x.key) {k=j; x=a[j];}
                 a[k]=a[i]; a[i]=x;
-                }
             }
         }
-    void BubbleSort(item a) {}
+    }
+    void BubbleSort(item a) {
+        item x;
+        for (int i = 2; i < n; i++)
+            for (int j = n; j > i; k--)
+                if (a[j].key < a[j-1].key) {
+                    x = a[j - 1];
+                    a[j - 1] = a[j];
+                    a[j] = x;
+                }
+    }
     void ShakerSort(item a) {}
     void QuickSort(item a) {
-        int i,j; item
+        int i,j; 
+        item b;
     }
-    public static void main String[] args {
+    public static void main(String[] args) {
         item()[10000] sorted, random, opposite;
+        //СТАДИЯ 1. ВВОД
         for (int i=0; i<10000; i++)
         {
             sorted[i].key=i;
@@ -49,6 +60,9 @@ public class CompareThemAll {
             opposite[i].password=sorted[i].password;
             random[i].password=sorted[i].password;
         }
+        //СТАДИЯ 2. СОРТИРОВКА
+        System.out.println(opposite[i].key,opposite[i].password);
+        BubbleSort(opposite);
+        System.out.println(opposite[i].key,opposite[i].password);
     }
 }
-
