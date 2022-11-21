5ab80a7e1361f7d1b26b62c605de318975cc4957
DagonChernovski <95167373+DagonChernovski@users.noreply.github.com>
2022-11-21 13:43:15 +0500

    Create CompareThemAll.java

---
+++ CompareThemAll.java
@@ -0,0 +1,54 @@
+import java.lang.String;
+import java.lang.Math;
+
+public class Comparethemall {
+    public class item {
+        public int key;
+        char[50] password;
+        public item () {
+            this.key=0;
+            this.password="_"*50;
+        }
+    }
+    void InsertSort(item a) {
+        int j, n=10000;
+        for (int i=2; i<n; i++) {
+            x=a[i]; a[0]=x; j:=i-1;
+            while (x.key<a[j].key) {
+                a[j+1]=a[j];
+                j=j-1;
+            }
+            a[j+1]=x;
+        }
+    };
+    void SelectionSort(item a) {
+        int j, k, n=10000;
+        for (int i=1; i<n-1; i++) {
+            k=i; x=a[i];
+            for (j=i+1; j<n; j++) {
+                if (a[j].key<x.key) {k=j; x=a[j];}
+                a[k]=a[i]; a[i]=x;
+                }
+            }
+        }
+    void BubbleSort(item a) {}
+    void ShakerSort(item a) {}
+    void QuickSort(item a) {
+        int i,j; item
+    }
+    public static void main String[] args {
+        item()[10000] sorted, random, opposite;
+        for (int i=0; i<10000; i++)
+        {
+            sorted[i].key=i;
+            opposite[i].key=10000-i;
+            random[i].key=Math.random(100000);
+            for (int j=0; j<50; j++) {
+                sorted[i].password[j] = (char) (Math.random(79) + 43);
+            }
+            opposite[i].password=sorted[i].password;
+            random[i].password=sorted[i].password;
+        }
+    }
+}
+
