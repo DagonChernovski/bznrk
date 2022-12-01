f90498d697514adea3d91b837495782557d6bccd
DagonChernovski <95167373+DagonChernovski@users.noreply.github.com>
2022-12-01 15:30:06 +0500

    Update EfficientSorting.java

--- EfficientSorting.java
+++ EfficientSorting.java
@@ -11,13 +11,8 @@ public class EfficientSorting {
         public String getPass() //{return this.password;}
         {String ret=""; for (int i = 0; i<50; i++) ret=ret+password[i]; return ret;}
     }
-    /*public<Type> String PrintArray(int a, int b, Type arr[]) {\
-        String ret;
-        for (int i=a; i<b; i++) {
-            System.out.print(arr[i]);
-        }
-    }*/
-    void InsertSort(item a[]) {
+
+    static void InsertSort(item a[]) {
         int j, n=10000;
         item x;
         for (int i=2; i<n; i++) {
@@ -28,11 +23,11 @@ public class EfficientSorting {
             }
             a[j+1]=x;
         }
-    };
+    }
     static void SelectionSort(item a[]) {
         int j, k, n=10000;
         item x;
-        for (int i=1; i<n-1; i++) {
+        for (int i=0; i<n-1; i++) {
             k=i; x=a[i];
             for (j=i+1; j<n; j++) {
                 if (a[j].key<x.key) {k=j; x=a[j];}
@@ -46,11 +41,7 @@ public class EfficientSorting {
         for (int i = 1; i < n; i++)
             for (int j = n-1; j > i; j--)
                 if (a[j].key < a[j-1].key) {
-                    //System.out.print("Switches ["+(j-1)+']'+a[j-1].getKey()+" and "+a[j].getKey());
-                    //if (Math.abs(5000-j)<=4995)
-                    //    for (int ii=-4; ii<5; ii++)
-                   //         System.out.print(a[j+ii].getKey()+" ");
-                   // System.out.println("    key+-4: "+j);
+
                     x = a[j - 1];
                     a[j - 1] = a[j];
                     a[j] = x;
@@ -58,11 +49,24 @@ public class EfficientSorting {
         return a;
     }
     void ShakerSort(item a) {}
-    /*void QuickSort(item a, int l, int r) {
+    void QuickSort(item a[], int l, int r) {
         int i=l,j=r;
         item b;
         item x=new item();
-        x.setItem((a[l+r].getKey()),a.getPass());
+        x.setItem((a[l+r].getKey()/2),a[l+r].getPass());
         while (a[i].key<x.key) {i=i+1;}
-    }*/
+    }
 }
+
+//System.out.print("Switches ["+(j-1)+']'+a[j-1].getKey()+" and "+a[j].getKey());
+//if (Math.abs(5000-j)<=4995)
+//    for (int ii=-4; ii<5; ii++)
+//         System.out.print(a[j+ii].getKey()+" ");
+// System.out.println("    key+-4: "+j);
+
+/*public<Type> String PrintArray(int a, int b, Type arr[]) {\
+        String ret;
+        for (int i=a; i<b; i++) {
+            System.out.print(arr[i]);
+        }
+    }*/
