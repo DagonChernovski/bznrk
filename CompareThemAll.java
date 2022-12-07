bf189c7414b03b4a1b90594cb6f4f677d95f5ed5
DagonChernovski <95167373+DagonChernovski@users.noreply.github.com>
2022-12-08 00:35:31 +0600

    Update CompareThemAll.java

--- CompareThemAll.java
+++ CompareThemAll.java
@@ -1,5 +1,3 @@
-package com.company;
-
 import java.lang.String;
 import java.lang.Math;
 import java.util.Random;
@@ -73,51 +71,45 @@ public class CompareThemAll extends EfficientSorting {
         commits[1][2]=SelectionSort(opposite);
         endTime=System.nanoTime();
         timeElapsed[1][2]=endTime-startTime;
-        for (int i=0; i<3; i++) System.out.println(timeElapsed[1][i]+" "+commits[1][i]);
         System.arraycopy(sorted_, 0, sorted, 0, 10000);
         System.arraycopy(random_, 0, random, 0, 10000);
         System.arraycopy(opposite_, 0, opposite, 0, 10000);
-
-
-
         System.out.println("Bubble Sorting:\n");
         //for (int i=0; i<10000; i++) System.out.println(random[i].getKey()+" "+random[i].getPass());
         startTime=System.nanoTime();
-        commits[2][0]=BubbleSort(sorted);
+        BubbleSort(sorted);
         endTime=System.nanoTime();
         timeElapsed[2][0]=endTime-startTime;
         startTime=System.nanoTime();
-        commits[2][1]=BubbleSort(random);
+        BubbleSort(random);
         endTime=System.nanoTime();
         timeElapsed[2][1]=endTime-startTime;
         //for (int i=0; i<10000; i++) System.out.println(random[i].getKey()+" "+random[i].getPass());
         startTime=System.nanoTime();
-        commits[2][2]=BubbleSort(opposite);
+        BubbleSort(opposite);
         endTime=System.nanoTime();
         timeElapsed[2][2]=endTime-startTime;
-        for (int i=0; i<3; i++) System.out.println(timeElapsed[2][i]+" "+commits[2][i]);
         //for (int i=0; i<10000; i++) System.out.println(sorted[i].getKey()+"   "+sorted_[i].getKey()+"  "+
-         //       (sorted_[i].getKey()==sorted[i].getKey())+"               "
-         //       +opposite[i].getKey()+"   "+opposite_[i].getKey()+"  "+((10001-opposite_[i].getKey())==opposite[i].getKey()));
+        //       (sorted_[i].getKey()==sorted[i].getKey())+"               "
+        //       +opposite[i].getKey()+"   "+opposite_[i].getKey()+"  "+((10001-opposite_[i].getKey())==opposite[i].getKey()));
         System.arraycopy(sorted_, 0, sorted, 0, 10000);
         System.arraycopy(random_, 0, random, 0, 10000);
         System.arraycopy(opposite_, 0, opposite, 0, 10000);
 
         System.out.println("Shaker sorting:\n");
         startTime=System.nanoTime();
-        commits[3][1]=ShakerSort(random);
+        ShakerSort(random);
         endTime=System.nanoTime();
         timeElapsed[3][1]=endTime-startTime;
         startTime=System.nanoTime();
-        commits[3][0]=ShakerSort(sorted);
+        ShakerSort(sorted);
         endTime=System.nanoTime();
         timeElapsed[3][0]=endTime-startTime;
         //for (int i=0; i<10000; i++) System.out.println(random[i].getKey()+" "+random[i].getPass());
         startTime=System.nanoTime();
-        commits[3][2]=ShakerSort(opposite);
+        ShakerSort(opposite);
         endTime=System.nanoTime();
         timeElapsed[3][2]=endTime-startTime;
-        for (int i=0; i<3; i++) System.out.println(timeElapsed[3][i]+" "+commits[3][i]);
         //for (int i=0; i<10000; i++) System.out.println(sorted[i].getKey()+"   "+sorted_[i].getKey()+"  "+
         //       (sorted_[i].getKey()==sorted[i].getKey())+"               "
         //       +opposite[i].getKey()+"   "+opposite_[i].getKey()+"  "+((10001-opposite_[i].getKey())==opposite[i].getKey()));
@@ -141,13 +133,20 @@ public class CompareThemAll extends EfficientSorting {
         QuickSort(opposite, 0, 9999);
         endTime=System.nanoTime();
         timeElapsed[3][2]=endTime-startTime;
-        for (int i=0; i<3; i++) System.out.println(timeElapsed[3][i]);//+" "+commits[3][i]);
+        //+" "+commits[3][i]);
         //for (int i=0; i<10000; i++) System.out.println(sorted[i].getKey()+"   "+sorted_[i].getKey()+"  "+
         //       (sorted_[i].getKey()==sorted[i].getKey())+"               "
         //       +opposite[i].getKey()+"   "+opposite_[i].getKey()+"  "+((10001-opposite_[i].getKey())==opposite[i].getKey()));
         System.arraycopy(sorted_, 0, sorted, 0, 10000);
         System.arraycopy(random_, 0, random, 0, 10000);
         System.arraycopy(opposite_, 0, opposite, 0, 10000);
+        //ВЫВОД ВСЕХ ДАННЫХ!!!
+        String sorting_type[]={"InsertSort","SelectionSort","BubbleSort","ShakerSort","QuickSort"};
+        for (int i=0; i<6; i++) {
+            System.out.println(sorting_type[i]);
+            for (int j = 0; j < 3; j++)
+                System.out.println(timeElapsed[i][j]);
+        }
     }
 
 }
