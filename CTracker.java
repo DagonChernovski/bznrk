03265a03f5b454d6dcd0208f6092fae65036e094
DagonChernovski <95167373+DagonChernovski@users.noreply.github.com>
2022-12-12 11:39:20 +0500

    Update CTracker.java

--- CTracker.java
+++ CTracker.java
@@ -1,5 +1,7 @@
+package com.company;
+
 public class CTracker extends EfficientSorting {
-    static int[] InsertSortC (item a[]) {
+    static long[] InsertSortC (item a[]) {
         int j, n=a.length,assign=0,compare=0;
         item x;
         for (int i=2; i<n; i++) {
@@ -12,72 +14,81 @@ public class CTracker extends EfficientSorting {
             a[j+1]=x;
             compare++; assign+=4;
         }
-        int c[]=new int[2];
+        long c[]=new long[2];
         c[0]=compare;c[1]=assign;
         return c;
     }
-    static int[] SelectionSortC(item a[]) {
+    static long[] SelectionSortC(item a[]) {
         int j, k, compare=0,assign=0, n=a.length;
         item x;
         for (int i=0; i<n; i++) {
             k=i; x=a[i]; assign+=3; compare++;
             for (j=i+1; j<n; j++) {
                 if (a[j].key<x.key) {k=j; x=a[j]; assign+=2;}
-                a[k]=a[i]; a[i]=x; compare+=2; assign+=3;
+                a[k]=a[i]; a[i]=x; compare++; assign+=3;
             }
+            //System.out.printf("A %d C %d\n",assign,compare);
         }
-        int c[]=new int[2];
+        long c[]=new long[2];
         c[0]=compare; c[1]=assign;
         return c;
     }
-    static int[] BubbleSortC(item a[]) {
+    static long[] BubbleSortC(item a[]) {
         item x;
         int n=a.length, compare=0,assign=0;
-        for (int i = 0; i < n; i++)
-            for (int j = n-1; j > i; j--)
-                if (a[j].key < a[j-1].key) {
+        for (int i = 0; i < n; i++) {
+            for (int j = n - 1; j > i; j--) {
+                if (a[j].key < a[j - 1].key) {
                     x = a[j - 1];
                     a[j - 1] = a[j];
                     a[j] = x;
-                    assign+=3;
+                    assign += 3;
                 }
-        int c[]=new int[2];
+                compare += 2;
+                assign += 2;
+            }
+            assign++;
+            compare++;
+        }
+        long c[]=new long[2];
         c[0]=compare;c[1]=assign;
         return c;
     }
-    static int[] ShakerSortC(item a[]) {
-        int k=0, l=0, compare=0,assign=0, r=a.length;
+    static long[] ShakerSortC(item a[]) {
+        int k=0, l=0, compare=0,assign=0, r=a.length-1;
         item x;
         boolean was;
         do {
-            was = false;
-            for (int i = r - 1; i > l; i--)
+            was=false;
+            for (int i = r; i > l; i--)
                 if (a[i - 1].getKey() > a[i].getKey()) {
                     x = a[i - 1];
                     a[i - 1] = a[i];
                     a[i] = x;
                     k = i;
-                    assign+=4;
-                    was=true;
-                } while (!was);
-            l = k + 1;
-            for (int i = l; i < r; i++)
+                    assign += 4;
+                    compare++;
+                    was = true;
+                }
+            l = k+1;
+            for (int i = l; i <= r; i++)
                 if (a[i - 1].getKey() > a[i].getKey()) {
                     x = a[i - 1];
                     a[i - 1] = a[i];
                     a[i] = x;
                     k = i;
-                    assign+=4; compare++;
-                    was=true;
+                    assign += 4;
+                    compare++;
+                    was = true;
                 }
             compare+=2; assign+=5;
-            r = k - 1;
+            r = k;
         } while (was);
-        int c[]=new int[2];
+        long c[]=new long[2];
         c[0]=compare;c[1]=assign;
         return c;
     }
-    static int[] QuickSortC(item a[], int l, int r) {
+    static long[] QuickSortC(item a[], int l, int r) {
         int i=l,j=r,compare=0,assign=0;
         item w;//=new item();
         int middle=l+(r-l)/2;
@@ -96,8 +107,8 @@ public class CTracker extends EfficientSorting {
                 assign+=5; compare++;
             }
         } while (i<j);
-        int _c[]=new int[2];
-        int c[]=new int[2];
+        long _c[]=new long[2];
+        long c[]=new long[2];
         if (l<j) _c=QuickSortC(a, l, j);
         c[0]+=_c[0]; c[1]+=_c[1];
         if (i<r) _c=QuickSortC(a, i, r);
