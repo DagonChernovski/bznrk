6d9727b4c73e649774b24d68ed3d3d488c771960
DagonChernovski <95167373+DagonChernovski@users.noreply.github.com>
2022-12-19 13:49:58 +0500

    Update CTracker.java

--- CTracker.java
+++ CTracker.java
@@ -18,11 +18,11 @@ public class CTracker extends EfficientSorting {
         c[0]=compare;c[1]=assign;
         return c;
     }
-    static long[] SelectionSortC(item a[]) {
+    static long[] SelectionSortC(item a[]) { //без барьера
         int j, k, compare=0,assign=0, n=a.length;
         item x;
         for (int i=0; i<n; i++) {
-            k=i; x=a[i]; assign+=3; compare++;
+            k=i; x=a[i]; assign++;
             for (j=i+1; j<n; j++) {
                 if (a[j].key<x.key) {k=j; x=a[j]; assign+=2;}
                 a[k]=a[i]; a[i]=x; compare++; assign+=3;
@@ -36,19 +36,14 @@ public class CTracker extends EfficientSorting {
     static long[] BubbleSortC(item a[]) {
         item x;
         int n=a.length, compare=0,assign=0;
-        for (int i = 0; i < n; i++) {
-            for (int j = n - 1; j > i; j--) {
+        for (int i = 1; i < n; i++) {
+            for (int j = n - 1; j >= i; j--) {
                 if (a[j].key < a[j - 1].key) {
-                    x = a[j - 1];
-                    a[j - 1] = a[j];
-                    a[j] = x;
+                    x = a[j - 1]; a[j - 1] = a[j]; a[j] = x;
                     assign += 3;
                 }
-                compare += 2;
-                assign += 2;
+                compare++;
             }
-            assign++;
-            compare++;
         }
         long c[]=new long[2];
         c[0]=compare;c[1]=assign;
@@ -61,7 +56,7 @@ public class CTracker extends EfficientSorting {
         do {
             was=false;
             for (int i = r; i > l; i--)
-                if (a[i - 1].getKey() > a[i].getKey()) {
+                if (a[i - 1].key > a[i].key) {
                     x = a[i - 1];
                     a[i - 1] = a[i];
                     a[i] = x;
@@ -72,7 +67,7 @@ public class CTracker extends EfficientSorting {
                 }
             l = k+1;
             for (int i = l; i <= r; i++)
-                if (a[i - 1].getKey() > a[i].getKey()) {
+                if (a[i - 1].key > a[i].key) {
                     x = a[i - 1];
                     a[i - 1] = a[i];
                     a[i] = x;
@@ -95,16 +90,16 @@ public class CTracker extends EfficientSorting {
         item pivot=a[middle];
         assign+=4;
         do {
-            while (a[i].getKey()<pivot.getKey()) {i++;}
-            while (pivot.getKey()<a[j].getKey()) {j--;}
-            compare+=(assign+=(i-l)+(r-j));
+            while (a[i].key<pivot.key) {i++;}
+            while (pivot.key<a[j].key) {j--;}
+            compare+=(i-l)+(r-j);
             if (i<=j) {
                 w = a[i];
                 a[i] = a[j];
                 a[j] = w;
                 i++;
                 j--;
-                assign+=5; compare++;
+                assign+=3;
             }
         } while (i<j);
         long _c[]=new long[2];
@@ -114,6 +109,49 @@ public class CTracker extends EfficientSorting {
         if (i<r) _c=QuickSortC(a, i, r);
         c[0]+=_c[0]; c[1]+=_c[1];
         c[0]+=compare;c[1]+=assign;
+        return c;
+    }
+    static long[] siftC(item a[], int n, int i) {
+        int max = i,compare=0,assign=0;
+        int l = 2*i + 1;
+        int r = 2*i + 2;
+        if (l < n && a[l].key > a[max].key)
+        {max = l; assign++;}
+        if (r < n && a[r].key > a[max].key)
+        { max = r; assign++;}
+        long _c[]=new long[2];
+        if (max != i)
+        {
+            item swap = a[i];
+            a[i] = a[max];
+            a[max] = swap;
+            _c=siftC(a, n, max);
+        }
+        compare+=5;
+        long[] c =new long[2];
+        c[0]+=_c[0]; c[1]+=_c[1];
+        c[0]+=compare;c[1]+=assign;
+        return c;
+    }
+    static long[] PyramidalSortC(item a[]) {
+        item temp;
+        int size = a.length-1;
+        long[] c =new long[2];
+        long[] _c;
+        for (int i = (a.length / 2); i >= 0; i--) {
+            _c=siftC(a, size,i);
+            c[0]+=_c[0]; c[1]+=_c[1];
+            c[0]++; c[1]++;
+        };
+        for(int i=size; i>=0; i--) {
+            temp = a[0];
+            a[0] = a[size];
+            a[size] = temp;
+            size--;
+            _c=siftC(a, size,0);
+            c[0]+=_c[0]; c[1]+=_c[1];
+            c[0]++; c[1]+=5;
+        }
         return c;
     }
 }
