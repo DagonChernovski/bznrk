0d6cbcf8b95ce9d29a7995e0b8eefd8b354fc94d
DagonChernovski <95167373+DagonChernovski@users.noreply.github.com>
2023-05-12 15:35:36 +0500

    Update g.java

--- g.java
+++ g.java
@@ -1,73 +1,71 @@
-package com.company;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 
 interface function { double calc(double x);}
-class RectangleMethod extends Thread{
-    int number,count;
+class RectangleMethod extends Thread {
+    int threadNum,precision;
     double a,b,h=0;
     function f;
-    RectangleMethod(int num1, int num2, double _a, double _b, function func){
-        number = num1;
-        count = num2;
+    RectangleMethod(int _threadNo, int _p, double _a, double _b, function func) {
+        threadNum = _threadNo;
+        precision = _p;
         a = _a;
         b = _b;
         f=func;
     }
     @Override
     public void run(){
-        h = (b - a) / count;
+        h = (b - a) / precision;
         double x = 0;
-        for (int i = number - 1; i < count; i += Main.numthreads) {
+        for (int i = threadNum - 1; i < precision; i += Main.numthreads) {
             x = a + h * i;
-            Main.sumarray[number - 1] += (h * f.calc(x));
+            Main.sumarray[threadNum - 1] += (h * f.calc(x));
         }
     }
 }
-class TrapezoidMethod extends Thread{
-    int number,count;
+class TrapezoidMethod extends Thread {
+    int threadNum,precision;
     double a,b,h=0;
     function f;
-    TrapezoidMethod(int num1, int num2, double _a, double _b, function func){
-        number = num1;
-        count = num2;
+    TrapezoidMethod(int _threadNo, int _p, double _a, double _b, function func) {
+        threadNum = _threadNo;
+        precision = _p;
         a = _a;
         b = _b;
         f=func;
     }
     @Override
     public void run(){
-        h = (b - a) / count;
+        h = (b - a) / precision;
         double x = 0;
-        if (number == 1) Main.sum += (f.calc(a)+ f.calc(a + h * count))/2;
-        for (int i = number - 1; i < count - 1; i += Main.numthreads) {
+        if (threadNum == 1) Main.sum += (f.calc(a)+ f.calc(a + h * precision))/2;
+        for (int i = threadNum - 1; i < precision - 1; i += Main.numthreads) {
             x = a + h * i;
-            Main.sumarray[number - 1] += (f.calc(x));
+            Main.sumarray[threadNum - 1] += (f.calc(x));
         }
     }
 }
 
 class SimpsonMethod extends Thread{
-    int number,count;
+    int threadNum,precision;
     double a,b,h=0;
     function f;
-    SimpsonMethod(int num1, int num2, double _a, double _b, function func) {
-        number = num1;
-        count = num2;
+    SimpsonMethod(int _threadNo, int _p, double _a, double _b, function func) {
+        threadNum = _threadNo;
+        precision = _p;
         a = _a;
         b = _b;
         f = func;
     }
     @Override
     public void run(){
-        h = (b - a) / count;
+        h = (b - a) / precision;
         double x = 0;
-        if (number == 1) Main.sum += f.calc(a) + f.calc(b);
-        for (int i = number - 1; i < count - 1; i += Main.numthreads) {
+        if (threadNum == 1) Main.sum += f.calc(a) + f.calc(b);
+        for (int i = threadNum - 1; i < precision - 1; i += Main.numthreads) {
             x = a + h * i;
-            if (i % 2 == 0) Main.sumarray[number - 1] += (f.calc(x));
-            else Main.sumarray2[number - 1] += (f.calc(x));
+            if (i % 2 == 0) Main.sumarray[threadNum - 1] += (f.calc(x));
+            else Main.sumarray2[threadNum - 1] += (f.calc(x));
         }
     }
 }
@@ -83,11 +81,12 @@ public class Main {
     static double[] sumarray2 = new double[numthreads];
     static function ff=(x)->(Math.pow(Math.log(x),2)/x); // function interface
     static double a=1, b=4; //integral edges
-    static final int precision=(int)1e8;
+    static final int PRECISION=(int)1e8;
+
     public static double sum1 = 0, sum2 = 0;
     public static void main(String[] args) {
         System.out.println("-----------------------------------");
-        System.out.println("Rectangle Method");
+        System.out.println("Метод прямоугольника");
         System.out.println("-----------------------------------");
 
         //(Math.pow(Math.E, x)*(1 + Math.sin(x))) / (1 + Math.cos(x));
@@ -98,7 +97,7 @@ public class Main {
             sumarray = new double[numthreads];
             long starttime = System.currentTimeMillis();
             for (int i = 0; i < numthreads; i++) {
-                ThreadArr.add(i, new RectangleMethod(i + 1, precision, a, b, ff));
+                ThreadArr.add(i, new RectangleMethod(i + 1, PRECISION, a, b, ff));
                 //ThreadArr[i].setName("Thread-" + i);
                 (ThreadArr.get(i)).start();
             }
@@ -113,22 +112,22 @@ public class Main {
             for (int x = 0; x < sumarray.length; x++) sum += sumarray[x];
             long endtime = System.currentTimeMillis();
             double elapsed = endtime - starttime ;
-            System.out.println("Working time: " + elapsed / 1000 + "s., Threads num = " + numthreads);
+            System.out.println("Время работы: " + elapsed / 1000 + "с., потоков = " + numthreads);
             System.out.println(sum);
             System.out.println("-----------------------------------");
         }
         System.out.println("-----------------------------------");
-        System.out.println("Trapezoid Method");
+        System.out.println("Метод трапеции");
         System.out.println("-----------------------------------");
 
         Arrays.fill(sumarray, 0);
-        for (int j = 0; j < threadnum.length ; j++) {
+        for (int j = 0; j < threadnum.length; j++) {
             sum = 0;
             numthreads = threadnum[j];
             sumarray = new double[numthreads];
             long starttime = System.currentTimeMillis();
             for (int i = 0; i < numthreads; i++) {
-                ThreadArr.add(i, new TrapezoidMethod(i + 1, precision, a, b, ff));
+                ThreadArr.add(i, new TrapezoidMethod(i + 1, PRECISION, a, b, ff));
                 //ThreadArr[i].setName("Thread-" + i);
                 ThreadArr.get(i).start();
             }
@@ -143,15 +142,15 @@ public class Main {
             }
 
             for (int x = 0; x < sumarray.length; x++) sum += sumarray[x];
-            sum *= (3./precision);
+            sum *= (3./PRECISION);
             long endtime = System.currentTimeMillis();
             double elapsed = endtime - starttime ;
-            System.out.println("Working time: " + elapsed / 1000 + "s.");
+            System.out.println("Время работы: " + elapsed / 1000 + "с.");
             System.out.println(sum);
             System.out.println("-----------------------------------");
         }
         System.out.println("-----------------------------------");
-        System.out.println("Simpson Method");
+        System.out.println("Метод Симпсона");
         System.out.println("-----------------------------------");
 
         Arrays.fill(sumarray, 0);
@@ -166,7 +165,7 @@ public class Main {
             sumarray2 = new double[numthreads];
             long starttime = System.currentTimeMillis();
             for (int i = 0; i < numthreads; i++) {
-                ThreadArr.add(i, new SimpsonMethod(i + 1, precision, a, b, ff));
+                ThreadArr.add(i, new SimpsonMethod(i + 1, PRECISION, a, b, ff));
                 //ThreadArr[i].setName("Thread-" + i);
                 ThreadArr.get(i).start();
             }
@@ -183,10 +182,10 @@ public class Main {
             for (int x = 0; x < sumarray2.length; x++) sum2 += sumarray2[x];
             sum += sum1 * 2;
             sum += sum2 * 4;
-            sum *= ((3./precision) / 3);
+            sum *= (((b-a)/PRECISION) / 3);
             long endtime = System.currentTimeMillis();
             double elapsed = endtime - starttime ;
-            System.out.println("Working time: " + elapsed / 1000 + "s.");
+            System.out.println("Время работы: " + elapsed / 1000 + "с.");
             System.out.println(sum);
             System.out.println("-----------------------------------");
         }
