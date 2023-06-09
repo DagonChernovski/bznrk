d68bb9ac0f64c9d18f3b7118e77b804e2525cc45
DagonChernovski <95167373+DagonChernovski@users.noreply.github.com>
2023-06-09 15:09:39 +0500

    Update File.java

--- Task3/File.java
+++ Task3/File.java
@@ -2,10 +2,11 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class File {
-    public static final FileWriter writer;
+    public static final FileWriter writer, aWriter;
     static {
         try {
-            writer = new FileWriter("Alphabet.txt", false);
+            writer=new FileWriter("Threader.txt",false);
+            aWriter = new FileWriter("Alphabet.txt", false);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
